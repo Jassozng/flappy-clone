3b7a6942811ea46aa0414c7c7ac16f8d4732305d
jasso <jassozng@gmail.com>
2022-10-10 04:41:56 -0500

    Flappy v1

---
+++ gameConfig.js
@@ -0,0 +1,25 @@
+import FlappyBirdScene from './js/FlappyBirdScene.js';
+
+window.onload = function(){
+    const config = {
+        type: Phaser.AUTO,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: {
+                    y: 600
+                },
+                debug: false
+            }
+        },
+        scale: {
+            mode: Phaser.Scale.FIT,
+            width: 288,
+            height: 512
+        },
+        scene : [ FlappyBirdScene ]
+    };
+
+    const game = new Phaser.Game(config);
+}
+
